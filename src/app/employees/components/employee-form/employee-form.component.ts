6779592a4bb8993d7f2d3ecfffa51896cf4716a4
Christian <christianjguevara@hotmail.com>
2021-12-22 02:52:32 -0400

    for employee

--- src/app/employees/components/employee-form/employee-form.component.ts
+++ src/app/employees/components/employee-form/employee-form.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthService } from '@core/services/auth.service';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MyValidators } from '@utils/validators';
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
@@ -8,6 +15,8 @@ import { Router } from '@angular/router';
 })
 export class EmployeeFormComponent implements OnInit {
   isDisabled: boolean = true;
+  form: FormGroup;
+
   vaccineType = [
     { value: 'none', viewValue: 'Seleccion un tipo de vacuna' },
     { value: 'sputnik', viewValue: 'Sputnik' },
@@ -16,9 +25,115 @@ export class EmployeeFormComponent implements OnInit {
     { value: 'J&J', viewValue: 'Jhonson&Jhonson' },
   ];
 
-  constructor(private router: Router) {}
+  validations = {
+    birthdate: [
+      {
+        type: 'required',
+        msg: 'Este campo es requerido',
+      },
+    ],
+    address: [
+      {
+        type: 'required',
+        msg: 'Este campo es requerido',
+      },
+      {
+        type: 'minlength',
+        msg: 'Debe ser de minimo 6 caracteres',
+      },
+      {
+        type: 'maxlength',
+        msg: 'debe ser de un maximo de 24 caracteres',
+      },
+    ],
+    dose: [
+      {
+        type: 'required',
+        msg: 'Este campo es requerido',
+      },
+      {
+        type: 'min',
+        msg: 'Debe ser de minimo 1',
+      },
+      {
+        type: 'max',
+        msg: 'Debe ser de un maximo de 3',
+      },
+    ],
+    telephone: [
+      {
+        type: 'required',
+        msg: 'Este campo es requerido',
+      },
+      {
+        type: 'minlength',
+        msg: 'Debe ser de minimo 6 caracteres',
+      },
+      {
+        type: 'maxlength',
+        msg: 'debe ser de un maximo de 24 caracteres',
+      },
+    ],
+    VaccineType: [
+      {
+        type: 'required',
+        msg: 'Este campo es requerido',
+      },
+    ],
+    vaccineDate: [
+      {
+        type: 'required',
+        msg: 'Este campo es requerido',
+      },
+    ],
+  };
+
+  constructor(
+    private router: Router,
+    private formBuilderService: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.form = this.formBuilder();
+  }
 
   ngOnInit(): void {}
+
+  private formBuilder() {
+    return this.formBuilderService.group({
+      birthdate: [{ value: '', disabled: true }, [Validators.required], []],
+      address: [
+        { value: '', disabled: true },
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(24),
+        ],
+      ],
+      dose: [
+        { value: 0, disabled: true },
+        ,
+        [Validators.required, Validators.min(1), Validators.max(3)],
+      ],
+      telephone: [
+        { value: '', disabled: true },
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(24),
+        ],
+      ],
+      vaccunation_status: [{ value: false, disabled: true }, []],
+      VaccineType: [
+        { value: '', disabled: true },
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(24),
+        ],
+      ],
+      vaccineDate: [{ value: '', disabled: true }, [Validators.required], []],
+    });
+  }
 
   edit() {
     this.isDisabled = !this.isDisabled;
@@ -31,5 +146,27 @@ export class EmployeeFormComponent implements OnInit {
   onSubmit(event: Event) {
     event.preventDefault();
     console.log('Employee updated');
+  }
+
+  get birthdateField(): AbstractControl {
+    return this.form.get('birthdate') as AbstractControl;
+  }
+  get addressField(): AbstractControl {
+    return this.form.get('address') as AbstractControl;
+  }
+  get telephoneField(): AbstractControl {
+    return this.form.get('telephone') as AbstractControl;
+  }
+  get vaccunationStatusField(): AbstractControl {
+    return this.form.get('vaccunation_status') as AbstractControl;
+  }
+  get VaccineTypeField(): AbstractControl {
+    return this.form.get('VaccineType') as AbstractControl;
+  }
+  get vaccineDateField(): AbstractControl {
+    return this.form.get('vaccineDate') as AbstractControl;
+  }
+  get doseField(): AbstractControl {
+    return this.form.get('dose') as AbstractControl;
   }
 }
--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 import { SharedModule } from '@shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EmployeeFormComponent],
-  imports: [CommonModule, EmployeesRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    EmployeesRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class EmployeesModule {}
